a9105ed2cc4af059e4d0f89135dd13ded53d5db8
1 <54403680+OBj56@users.noreply.github.com>
2024-05-13 07:48:41 -0400

    testpt2

--- test.lua
+++ test.lua
@@ -1,1 +1,34 @@
-print("Hello")
+for i,v in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
+    if v:IsA("Accessory") then v.Handle.OriginalSize:Remove()
+    end
+    end
+game.Players.LocalPlayer.Character.Humanoid.BodyDepthScale:Remove()
+wait(.2)
+for i,v in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
+    if v:IsA("Accessory") then v.Handle.OriginalSize:Remove()
+    end
+    end
+game.Players.LocalPlayer.Character.Humanoid.BodyTypeScale:Remove()
+wait(.2)
+for i,v in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
+    if v:IsA("Accessory") then v.Handle.OriginalSize:Remove()
+    end
+    end
+game.Players.LocalPlayer.Character.Humanoid.BodyProportionScale:Remove()
+wait(.2)
+for i,v in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
+    if v:IsA("Accessory") then v.Handle.OriginalSize:Remove()
+    end
+    end
+game.Players.LocalPlayer.Character.Humanoid.BodyHeightScale:Remove()
+wait(.2)
+for i,v in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
+    if v:IsA("Accessory") then v.Handle.OriginalSize:Remove()
+    end
+    end
+game.Players.LocalPlayer.Character.Humanoid.BodyWidthScale:Remove()
+wait(.2)
+for i,v in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
+    if v:IsA("Accessory") then v.Handle.OriginalSize:Remove()
+    end
+    end
